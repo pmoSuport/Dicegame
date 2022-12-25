aac2e367cc4fbef265cc22113ff9d1e0945a78c2
pmoSuport <pmospprt@gmail.com>
2022-12-25 22:28:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+
+
+let score  = 0;
+
+
+function gen(){
+
+    var img_dom = document.querySelector('.dice');
+    var dom_score = document.getElementById("score");
+ 
+    var num = Math.floor(Math.random()*6)+1 ;
+
+    img_dom.src="dice-"+num+".png" ;
+
+    score = score + num ;
+
+    dom_score.textContent = "Total score : " + (score) ;
+
+    
+    
+}
+
+function restart(){
+    var dom_score = document.getElementById("score");
+    dom_score.textContent = "Total score :  0";
+    score = 0;
+}
+
+
+
+
